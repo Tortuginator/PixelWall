d2b4275a22a376c3d54a0062c50986ebaf4146f1
Felix <felix.friedberger@gmail.com>
2016-12-25 13:10:02 +0100

    New Symbols & testscreen
    Added new Symbols
    Added Testscreen
    Added RandomPattern Generator
    Implemented Custom Animation function
    Performance Improvements
    Fixed Animation input criterie
    Various improvements

--- PixelWall.py
+++ PixelWall.py
@@ -1,5 +1,6 @@
 import time
 import math
+import random
 from PIL import Image
 class FrameObjects():
 	def __init__(self):
@@ -24,6 +25,7 @@ class RenderEngine():
 		self.frameTimes = [] #for Statistics
 		self.Animations = [] #All Animations are saved here
 		self.BitMapDB = {} #A temporary library of BMP maps
+		self.Storage = {}
 
 	def setFramePreset(self,NewPreset):
 		if NewPreset.instanceOf(Frame):
@@ -113,21 +115,48 @@ class RenderEngine():
 						else:
 							dFrame.setPixel(i["Position"]["X"]+x,i["Position"]["Y"]+y,(content[0][localOffset],content[1][localOffset],content[2][localOffset]))
 
-			if i["Type"] == "Circle":
+			elif i["Type"] == "TestA":
+				totalduration = 80
+				CurrentIndex = (pastFrames%totalduration)+1
+
+				if CurrentIndex <= 5:
+					for p in range(0,(self.height//2)+1):
+						dFrame.drawRectangle(0,self.width-1,p*2,p*2,(255,0,0))
+				elif CurrentIndex <= 10:
+					for p in range(0,(self.height//2)+1):
+						dFrame.drawRectangle(0,self.width-1,p*2,p*2,(0,255,0))
+				elif CurrentIndex <= 15:
+					for p in range(0,(self.height//2)+1):
+						dFrame.drawRectangle(0,self.width-1,p*2,p*2,(0,0,255))
+				elif CurrentIndex <= 20:
+					for p in range(0,(self.height//2)+1):
+						dFrame.drawRectangle(0,self.width-1,(p*2)+1,(p*2)+1,(255,0,0))
+				elif CurrentIndex <= 25:
+					for p in range(0,(self.height//2)+1):
+						dFrame.drawRectangle(0,self.width-1,(p*2)+1,(p*2)+1,(0,255,0))
+				elif CurrentIndex <= 30:
+					for p in range(0,(self.height//2)+1):
+						dFrame.drawRectangle(0,self.width-1,(p*2)+1,(p*2)+1,(0,0,255))
+				elif CurrentIndex <= 60:
+					CurrentIndex =CurrentIndex-30
+					dFrame.drawRectangle(0,CurrentIndex,0,self.height-1,(255,255,255))
+				elif CurrentIndex <=80:
+					CurrentIndex =CurrentIndex-60
+					dFrame.drawRectangle(0,self.width,0,self.height,((255/20)*CurrentIndex,(255/20)*CurrentIndex,(255/20)*CurrentIndex))
+
+			elif i["Type"] == "Circle":
 				Cindex = pastFrames%(i["Radius"])
 				Cindex = Cindex+1
 				baseColor = [i["Color"][0] - i["ColorGRAD"][0],i["Color"][1] - i["ColorGRAD"][1],i["Color"][2] - i["ColorGRAD"][2]]
 
 				for p in range(0,Cindex+1):
-					CurrentRadius = p
-					MaxRadius = Cindex
-					FadeIn = i["Length"]
+					CurrentRadius = p;MaxRadius = Cindex;FadeIn = i["Length"];
 
 					if MaxRadius-FadeIn <= CurrentRadius:
 						divRelation = float(CurrentRadius-MaxRadius+FadeIn)/float(FadeIn)
 					else:
 						divRelation = 0
-					print divRelation
+
 					if p > Cindex:
 						divRad = Cindex
 					else:
@@ -136,7 +165,26 @@ class RenderEngine():
 					dFrame.drawCircle(i["Position"]["X"],i["Position"]["Y"],divRad,divColor)
 
 				#def drawCircle(self, x0, y0, radius, colour):
+			elif i["Type"] == "Pattern":
+				Cindex = pastFrames-i["Start"]
+				if not "RND_PATT" in self.Storage:
+					self.Storage["RND_PATT"] = [];
+					tmLength = self.width*self.height
+					for p in range(0,tmLength+1):
+						self.Storage["RND_PATT"].append(random.randint(0,255))
 
+				tmLength = self.width*self.height
+				for p in range(0,tmLength+1):
+					x0 = p%self.width
+					y0 = p//self.width
+					c0r = (i["ColorA"][0] - i["ColorB"][0])*(-1)
+					c0g = (i["ColorA"][1] - i["ColorB"][1])*(-1)
+					c0b = (i["ColorA"][2] - i["ColorB"][2])*(-1)
+					p0 = math.cos(self.Storage["RND_PATT"][p]*float(Cindex*0.0001))
+					if p0 < 0:
+						p0 = p0*(-1)
+					c0 = (int(i["ColorA"][0] + c0r*p0),int(i["ColorA"][1] + c0g*p0),int(i["ColorA"][2] + c0b*p0))
+					dFrame.setPixel(x0,y0,c0)
 		r += 1;
 		return dFrame
 
@@ -159,6 +207,7 @@ class RenderEngine():
 		if not Static in [True,False]:return 0
 		if not Loop in [True,False]:return 0
 		if not Transparency in [True,False]: return 0;
+		if not StartFrame%1 == 0:return 0;#Even number
 
 		NewA = {"Type":"File","Factor":factor,"Position":{"X":X,"Y":Y},"Image":{"File":file},"Transparency":Transparency,"Frame":{"Height":height,"totalframes":ImageFrames},"Start":StartFrame,"Loop":Loop,"Static":Static};
 		if not NewA["Image"]["File"] in self.BitMapDB or NewA["Static"] == False:
@@ -166,6 +215,17 @@ class RenderEngine():
 		if not (self.BitMapDB[file]["height"]%NewA["Frame"]["Height"]) == 0:
 			return 0
 		self.Animations.append(NewA);
+		return 1
+
+	def addAnnimationTest(self,factor = 1,Loop = True,StartFrame = 0):
+		if not Loop in [True,False]:return 0
+		if not factor > 0:return 0
+		if not StartFrame >= 0:return 0
+		if not StartFrame%1 == 0:return 0;#Even number
+
+		newA = {"Type":"TestA","Factor":factor,"Start":StartFrame,"Loop":Loop}
+
+		self.Animations.append(newA);
 		return 1
 
 	def addAnnimationCircle(self,X,Y,radius,color,Loop = True,StartFrame = 0,colorGRAD = (0,0,0),factor = 1,length = 1):
@@ -180,6 +240,22 @@ class RenderEngine():
 
 		newA = {"Type":"Circle","Factor":factor,"Position":{"X":X,"Y":Y},"Radius":radius,"Color":color,"ColorGRAD":colorGRAD, "Start":StartFrame,"Loop":Loop,"Length":length}
 
+		self.Animations.append(newA);
+		return 1
+	def addAnnimationCustom(self,Loop=False,StartFrame = 1,duration = 1,function = None,factor = 1):
+		print "NOT IMPLEMENTED"
+		pass
+
+	def addAnimationPattern(self,Loop=True,StartFrame = 1,factor = 1,colorRangeA=(0,0,0),colorRangeB=(255,255,255),RGBCorrelated = False):
+		if not Loop in [True,False]:return 0
+		if not StartFrame >= 0:return 0
+		if not StartFrame%1 == 0:return 0;#Even number
+		if not factor > 0:return 0
+		if not Frame.isColor(colorRangeA):return 0
+		if not Frame.isColor(colorRangeB):return 0
+		if not RGBCorrelated in [True,False]:return 0
+
+		newA = {"Type":"Pattern","Factor":factor,"Start":StartFrame,"Loop":Loop,"ColorA":colorRangeA,"ColorB":colorRangeB,"RGBcorr":RGBCorrelated}
 		self.Animations.append(newA);
 		return 1
 
@@ -245,7 +321,7 @@ class Frame():
 			self.setPixel(x0 + y, y0 - x, colour)
 			self.setPixel(x0 - y, y0 - x, colour)
 
-	def setRectangle(self,Xa,Xb,Ya,Yb,color):
+	def drawRectangle(self,Xa,Xb,Ya,Yb,color):
 		if not self.isPixel(Xa,Ya):return 0;
 		if not self.isPixel(Xb,Yb):return 0;
 
@@ -315,24 +391,32 @@ class Frame():
 		return (int(color[0]),int(color[1]),int(color[2]))
 
 	def writeText(self,X,Y,text,color):
-		text = text.toLower();#Geht das?
 		if not Frame.isColor(color):return 0;
 		if not self.isPixel(X,Y):return 0;
-		spaces = {"!":1}
+		spaces = {"!":1,"|":1,":":1,".":1}
 		chars = {	"0":[(0,0),(2,0),(2,1),(2,2),(2,3),(2,4),(2,5),(1,5),(0,5),(0,4),(0,3),(0,2),(0,1),(1,0)],
 					"1":[(2,0),(2,1),(2,2),(2,3),(2,4),(2,5),(1,4),(0,3)],
 					"2":[(0,0),(1,0),(2,0),(0,1),(1,2),(2,3),(2,4),(1,5),(0,4)],
-					"3":[(0,0),(1,0),(2,1),(1,2),(2,4),(2,3),(1,5),(0,5)],
+					"3":[(0,0),(1,0),(2,1),(1,2),(2,3),(1,4),(0,4)],
 					"4":[(2,0),(2,1),(2,2),(1,2),(0,2),(0,3),(0,4),(0,5),(2,3),(2,4)],
 					"5":[(0,1),(1,0),(2,1),(1,3),(0,3),(0,4),(0,5),(1,5),(2,5),(2,2)],
 					"6":[(0,1),(0,3),(1,0),(2,1),(1,2),(0,4),(1,5),(2,5),(0,2)],
 					"7":[(0,0),(0,1),(1,2),(1,3),(2,4),(0,5),(1,5),(2,5)],
+					"8":[(0,0),(1,0),(2,0),(0,1),(2,1),(0,2),(1,2),(2,2),(0,3),(2,3),(0,4),(1,4),(2,4)],
+					"9":[(2,0),(2,1),(2,2),(2,3),(2,4),(1,4),(0,4),(0,3),(0,2),(1,2)],
 
 					"!":[(0,0),(0,2),(0,3),(0,4),(0,5)],
 					"a":[(0,0),(2,0),(2,1),(2,2),(2,3),(2,4),(2,5),(1,5),(0,5),(0,4),(0,3),(0,2),(0,1),(1,2)],
 					"b":[(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(1,0),(2,0),(1,2),(2,2),(2,1)],
 					"c":[(0,0),(0,1),(0,2),(1,0),(2,0),(1,2),(2,2)],
-					"d":[(0,0),(1,0),(2,0),(2,1),(2,2),(2,3),(2,4),(2,5),(0,1),(0,2),(1,2)]}
+					"d":[(0,0),(1,0),(2,0),(2,1),(2,2),(2,3),(2,4),(2,5),(0,1),(0,2),(1,2)],
+
+					"-":[(0,2),(1,2),(2,2)],
+					"_":[(0,0),(1,0),(2,0)],
+					":":[(0,1),(0,3)],
+					".":[(0,0)],
+					"|":[(0,0),(0,1),(0,2),(0,3),(0,4),(0,5)],
+					"+":[(1,1),(1,2),(1,3),(0,2),(2,2)]}
 		given = list(text)
 		pos = 0;
 		for i in text:
