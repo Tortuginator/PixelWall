f5cd8769482417c2dc7d7c8c27256763a68cfc3c
Felix <felix.friedberger@gmail.com>
2019-03-04 22:39:21 +0100

    Cleanup

--- PixelWall2/frame.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image, ImageDraw
-class Frame():
-    def __init__(self,width,height):
-        self.width = width
-        self.height = height
-        self.pilObject = Image.new("RGBA", (self.height, self.width), (0, 0, 0, 255));
-        
